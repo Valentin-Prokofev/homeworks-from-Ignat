eb9803d67239d55416e7f577ce09e9a9ace68fdc
Valentin-Prokofev <valentin.prokofbev@gmail.com>
2022-04-02 13:32:58 +0300

    hw5 without navlink

---
+++ src/p2-homeworks/h5-rrd-v6/Pages.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PreJunior from "./pages/PreJunior";
+import Error404 from "./pages/Error404";
+import {Routes, Route, Navigate} from 'react-router-dom';
+
+export const PATH = {
+    PRE_JUNIOR: '/pre-junior',
+    // add paths
+}
+
+function Pages() {
+    return (
+        <div>
+            {/*Routes выбирает первый подходящий роут*/}
+            <Routes>
+
+                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+                // add routes
+
+                {/*он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
+                <Route path={'/*'} element={<Error404/>}/>
+
+            </Routes>
+        </div>
+    )
+}
+
+export default Pages
