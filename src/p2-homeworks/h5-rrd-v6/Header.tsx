47a129905cb900a341813017c90b06c91cc0d7b2
Valentin-Prokofev <valentin.prokofbev@gmail.com>
2022-04-07 18:10:10 +0300

    5HW finished

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {NavLink} from "react-router-dom";
 import {PATH} from "./Pages";
 import s from "./Header.module.css"
 
 function Header() {
+    const [isClick, setIsClick] = useState(false)
+
+    const burgerChange = () => (isClick) ? setIsClick(false) : setIsClick(true)
+
     return (
         <div className={s.div}>
-            <NavLink to={PATH.PRE_JUNIOR}>pre-junior</NavLink>---
-            <NavLink to={PATH.JUNIOR}>junior</NavLink>---
-            <NavLink to={PATH.JUNIOR_PLUS}>junior-plus</NavLink>
+            <ul className={`${s.ul_navLink} ${isClick ? s.right : s.left}`}>
+                <li><NavLink to={PATH.PRE_JUNIOR}>pre-junior</NavLink></li>
+                <li><NavLink to={PATH.JUNIOR}>junior</NavLink></li>
+                <li><NavLink to={PATH.JUNIOR_PLUS}>junior-plus</NavLink></li>
+                <div className={s.square_div} onClick={burgerChange}></div>
+            </ul>
+            {/*<NavLink to={PATH.PRE_JUNIOR}>pre-junior</NavLink>---*/}
+            {/*<NavLink to={PATH.JUNIOR}>junior</NavLink>---*/}
+            {/*<NavLink to={PATH.JUNIOR_PLUS}>junior-plus</NavLink>*/}
             {/*<NavLink to={"/*"}></NavLink>*/}
         </div>
     )
