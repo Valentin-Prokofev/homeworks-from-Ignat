c608607ff890500444933e7cdff679b23a6d9903
Valentin-Prokofev <valentin.prokofbev@gmail.com>
2022-04-16 18:32:37 +0300

    week6

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -2,19 +2,21 @@ import React, {useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
+import s from './HW6.module.css'
 
 function HW6() {
-    const [value, setValue] = useState<string>('')
+    const [value, setValue] = useState<string>('')      //юстейт для локал стораж
 
-    const save = () => {
+    const save = () => {      //функция сохранения в локал стораж
         saveState<string>('editable-span-value', value)
     }
-    const restore = () => {
-        // setValue()
+    const restore = () => {   //функция доставания локал стораж
+        const state = restoreState<string>('editable-span-value', "blablabla")
+        setValue(state)
     }
 
     return (
-        <div>
+        <div className={s.main}>
             <hr/>
             homeworks 6
 
@@ -26,8 +28,10 @@ function HW6() {
                     spanProps={{children: value ? undefined : 'enter text...'}}
                 />
             </div>
-            <SuperButton onClick={save}>save</SuperButton>
-            <SuperButton onClick={restore}>restore</SuperButton>
+            <div className={s.buttons}>
+                <SuperButton onClick={save}>save</SuperButton>
+                <SuperButton onClick={restore}>restore</SuperButton>
+            </div>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
