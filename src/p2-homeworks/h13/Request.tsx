0da37e81137f86da61aad23d952ed0a5f85e2155
Valentin-Prokofev <valentin.prokofbev@gmail.com>
2022-06-04 18:43:39 +0300

    HW13

--- src/p2-homeworks/h13/Request.tsx
+++ src/p2-homeworks/h13/Request.tsx
@@ -1,16 +1,19 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {hw13API} from "./api/api";
 
 export const Request = () => {
-    let[checked, setChecked] = useState<boolean>(false)
+    let [checked, setChecked] = useState<boolean>(false)
 
     const onclickButtonHandler = () => {
-        hw13API.postSuccess(false)
+        hw13API.postSuccess(checked)
             .then((res: any) => {
                 console.log(res.data)
-                setChecked(true)
             })
-            .catch(err=>alert(err.message))
+            .catch(err => alert(err.message))
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setChecked(e.currentTarget.checked)
     }
 
     return (
@@ -18,7 +21,7 @@ export const Request = () => {
             <button onClick={onclickButtonHandler}>post</button>
             <input
                 type="checkbox"
-                onChange={()=>setChecked(true)}
+                onChange={onChangeHandler}
                 checked={checked}/>
         </div>
     );
--- src/p2-homeworks/h13/api/api.ts
+++ src/p2-homeworks/h13/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const hw13API = {
-    postSuccess: (success:boolean)=> {
-        return axios.post("https://neko-cafe-back.herokuapp.com/auth/test", {success} )
+    postSuccess: (success: boolean) => {
+        return axios.post("https://neko-cafe-back.herokuapp.com/auth/test", {success})
     }
 }
